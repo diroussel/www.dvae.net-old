f4a17f1c4ceaa90217096160e614b2fc4d7611cd
David Roussel <david.roussel@bjss.com>
2018-04-17 16:05:07 +0100

    allow unsafe-inline scripts in CSP

--- static.config.js
+++ static.config.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 export default {
   getRoutes: async () => {
-    const { data: posts } = await axios.get(
-      "https://jsonplaceholder.typicode.com/posts"
-    );
+    // const { data: posts } = await axios.get(
+    //   "https://jsonplaceholder.typicode.com/posts"
+    //);
     return [
       {
         path: "/",
@@ -14,20 +14,20 @@ export default {
         path: "/about",
         component: "src/containers/About"
       },
-      {
-        path: "/blog",
-        component: "src/containers/Blog",
-        getData: () => ({
-          posts
-        }),
-        children: posts.map(post => ({
-          path: `/post/${post.id}`,
-          component: "src/containers/Post",
-          getData: () => ({
-            post
-          })
-        }))
-      },
+      // {
+      //   path: "/blog",
+      //   component: "src/containers/Blog",
+      //   getData: () => ({
+      //     posts
+      //   }),
+      //   children: posts.map(post => ({
+      //     path: `/post/${post.id}`,
+      //     component: "src/containers/Post",
+      //     getData: () => ({
+      //       post
+      //     })
+      //   }))
+      // },
       {
         is404: true,
         component: "src/containers/404"
