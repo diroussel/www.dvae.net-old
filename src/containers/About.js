570202e018326899631de354800454bcbc321361
David Roussel <david.roussel@bjss.com>
2018-04-17 14:11:21 +0100

    Took out blog section

--- src/containers/About.js
+++ src/containers/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default () => (
   <div>
-    <h1>This is what we're all about.</h1>
-    <p>React, static sites, performance, speed. It's the stuff that makes us tick.</p>
+    <h1>This my website.</h1>
+    <p>Not much here, I wonder if there ever will be more?</p>
   </div>
 )
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,7 +4,6 @@ import logoImg from '../logo.png'
 
 export default () => (
   <div>
-    <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
-    <img src={logoImg} alt="" />
+    <h1 style={{ textAlign: 'center' }}>Welcome to www.dvae.net</h1>
   </div>
 )
