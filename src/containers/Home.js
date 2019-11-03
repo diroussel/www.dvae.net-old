1e83d25bda46603203d430bd037879c3ac72dcea
David Roussel <david.roussel@bjss.com>
2019-11-03 16:26:02 +0000

    Added ... to trigger build

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,6 +4,6 @@ import logoImg from '../logo.png'
 
 export default () => (
   <div>
-    <h1 style={{ textAlign: 'center' }}>Welcome to www.dvae.net</h1>
+    <h1 style={{ textAlign: 'center' }}>Welcome to www.dvae.net...</h1>
   </div>
 )
